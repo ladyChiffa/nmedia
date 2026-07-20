52500289ac9721bd708be029017ea2e7a3736e50
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-07-20 18:08:19 +0300

    ДЗ к занятию 2.3

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
@@ -26,44 +27,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         val viewModel: PostViewModel by viewModels<PostViewModel>()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                author.text = post.author
-                published.text = post.published
-                content.text = post.content
-                if (post.likedByMe) {
-                    likes?.setImageResource(R.drawable.ic_liked_24)
-                }
-                qLikes?.text = formatCount(post.likes)
-                qShares.text = formatCount(post.shares)
-                qViews.text = formatCount(post.views)
+        val adapter = PostsAdapter( {viewModel.likeById(it.id)},
+            {viewModel.shareById(it.id)})
 
-            }
-        }
+        binding.main.adapter = adapter
 
-        binding.likes?.setOnClickListener {
-            viewModel.like()
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
         }
-
-        binding.shares.setOnClickListener {
-            viewModel.share()
-        }
-    }
-
-    fun formatCount(count: Int) : String {
-        val ADD_SUFFIX_NONE = 1_000
-        val ADD_SUFFIX_KILO = 10_000
-        val ADD_SUFFIX_MEGA = 1_000_000
-
-        if(count < ADD_SUFFIX_NONE) return count.toString()
-        if(count < ADD_SUFFIX_KILO) {
-            val countShown = (count * 10 / ADD_SUFFIX_NONE).toDouble() / 10
-            return countShown.toString() + "K"
-        }
-        if(count < ADD_SUFFIX_MEGA) {
-            return (count / 1000).toString() + "K"
-        }
-        val countShown = (count * 10 / ADD_SUFFIX_MEGA).toDouble() / 10
-        return countShown.toString() + "M"
     }
 }
---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -0,0 +1,74 @@
+package ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+
+typealias PostListener = (Post) -> Unit
+
+class PostsAdapter (private val likeListener: PostListener, private val shareListener: PostListener)
+    : ListAdapter<Post, PostViewHolder>(PostDiffCallback) {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(
+            LayoutInflater.from(viewGroup.context),
+            viewGroup,
+            false
+        )
+        return PostViewHolder(binding, likeListener, shareListener)
+    }
+
+    override fun onBindViewHolder(viewHolder: PostViewHolder, position: Int) {
+        val post = getItem(position)
+        viewHolder.bind(post)
+    }
+}
+
+class PostViewHolder (private val binding: CardPostBinding,
+                      private val likeListener: PostListener,
+    private val shareListener: PostListener) : RecyclerView.ViewHolder(binding.root) {
+    fun bind (post: Post) {
+        with(binding) {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            likes?.setImageResource(if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24)
+            qLikes?.text = formatCount(post.likes)
+            qShares.text = formatCount(post.shares)
+            qViews.text = formatCount(post.views)
+            likes?.setOnClickListener {
+                likeListener(post)
+            }
+            shares.setOnClickListener {
+                shareListener(post)
+            }
+        }
+    }
+
+    fun formatCount(count: Int) : String {
+        val ADD_SUFFIX_NONE = 1_000
+        val ADD_SUFFIX_KILO = 10_000
+        val ADD_SUFFIX_MEGA = 1_000_000
+
+        if(count < ADD_SUFFIX_NONE) return count.toString()
+        if(count < ADD_SUFFIX_KILO) {
+            val countShown = (count * 10 / ADD_SUFFIX_NONE).toDouble() / 10
+            return countShown.toString() + "K"
+        }
+        if(count < ADD_SUFFIX_MEGA) {
+            return (count / 1000).toString() + "K"
+        }
+        val countShown = (count * 10 / ADD_SUFFIX_MEGA).toDouble() / 10
+        return countShown.toString() + "M"
+    }
+}
+
+object PostDiffCallback: DiffUtil.ItemCallback<Post>() {
+    override fun areItemsTheSame ( oldItem: Post, newItem: Post ) = oldItem.id == newItem.id
+    override fun areContentsTheSame ( oldItem: Post, newItem: Post ) = oldItem == newItem
+}
